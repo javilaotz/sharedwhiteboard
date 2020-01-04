cad6b5e090b4357478cab7369e0ab0d0a76bf15d
Jhon Avila <javilaortiz@gmail.com>
2020-01-04 11:23:38 -0500

    Completed Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,18 +7,18 @@ import './Styles.css'
 
 function Home(props) {
     return (
-        <Container className="themed-container" fluid={true}>
-            <Row>
-                <Col><NavBar/></Col>
-            </Row>
-            <Row className="WhiteBoard">
-                <Col md="10">Whiteboard</Col>
-                <Col md="2">Users</Col>
-            </Row>
-            <Row>
-                <Col>footer</Col>
-            </Row>
-        </Container>
+        <div>
+            <NavBar/>
+            <Container className="themed-container" fluid={true}>
+                <Row className="WhiteBoard">
+                    <Col md="10">Whiteboard</Col>
+                    <Col md="2">Users</Col>
+                </Row>
+                <Row>
+                    <Col>footer</Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  NavbarBrand
 } from 'reactstrap';
 
 const NavBar = (props) => {
@@ -19,16 +9,7 @@ const NavBar = (props) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
+        <NavbarBrand href="/">Shared Whiteboard</NavbarBrand>
       </Navbar>
     </div>
   );
