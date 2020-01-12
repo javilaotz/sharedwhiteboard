25ab7c871fa719d18bc176e73e738a4aebf5ad1a
Jhon Avila <javilaortiz@gmail.com>
2020-01-12 14:03:02 -0500

    whiteboard done

--- src/components/Whiteboard.jsx
+++ src/components/Whiteboard.jsx
@@ -1,81 +1,56 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import ReactDOM from "react-dom"
 import json_response from "../mock/response.json"
 
 const Whiteboard = (props) => {
-    useEffect(() => {
-        const context = canvas.current.getContext("2d");   
-        plotGrid(context, size, cellSize);
-        //plotCell(context, cellSize, {x:2, y:2})        
-        context.stroke()
-    });
-    
 
+    const [isMouseDown, setIsMouseDown] = useState(false);
+    let pos = {x :0, y:0}
+    
     const size = 500, /* Area of whiteboard*/
-          cellSize = 50,  /* Area of a cell */
           canvas = useRef(null),
+          line = {width : 1, color : '#ff0000'},
           resp = json_response && json_response
     
+    const startDrawing = (e, pos) => {
+        setIsMouseDown(true)
+        pos.x = e.clientX
+        pos.y = e.clientY
+    }
+
+    const finishDrawing = () => {
+        setIsMouseDown(false)
+    }
+
+    const drawLine = (e, canvas, pos, line) => {
+        const context = canvas.current.getContext("2d")
+        if(isMouseDown){
+            const newPos = {x : e.clientX, y : e.clientY}    
+
+            context.beginPath()
+            pos.x && pos.x !== 0 && pos.y && pos.y !== 0 && context.moveTo( pos.x, pos.y ) 
+            context.lineTo( newPos.x, newPos.y )
+            context.lineWidth = line.width
+            context.strokeStyle = line.color
+            pos.x = newPos.x
+            pos.y = newPos.y
+        }
+        context.stroke()
+    }
           
     return (
         <div>
-            <canvas ref={canvas} width={size} height={size} onClick={(e)=>trackClick(e, canvas, size, cellSize)}/>
+            <canvas 
+                className='canvas'
+                ref={canvas} 
+                width={size} 
+                height={size} 
+                onMouseMove={(e)=>drawLine(e, canvas, pos, line)} 
+                onMouseDown={(e)=>startDrawing(e, pos)} 
+                onMouseUp={()=>finishDrawing(pos)}
+            />
         </div>
     );
-}
-
-const trackClick = (e, canvas, size, cellSize) => {
-    console.log(e, "EVENTO")
-    const pos = {
-        x: e.clientX,
-        y: e.clientY
-      };
-    console.log(pos, "pos")
-
-    //calculating the cell clicked
-    const cell = {
-        x : 10*Math.floor((pos.x*cellSize)/size),
-        y : 10*Math.floor((pos.y*cellSize)/size)
-    }
-    console.log(cell, "cell ~")
-
-    //Plotting new points
-    plotPoint(canvas, cellSize, cell)
-
-
-}
-
-const plotPoint = (canvas, cellSize, location) => {
-    const x = location && location.x,
-          y = location && location.y,
-          ctx = canvas.current.getContext("2d")
-
-    ctx.save()    
-    ctx.fillStyle = "hsl(0, 0%, 0%)"
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "black";
-    ctx.fillRect(x, y, cellSize, cellSize)
-}
-
-const plotCell = (ctx, cellSize, location) => {
-    const x = location && location.x,
-          y = location && location.y
-    ctx.save()    
-    ctx.fillStyle = "hsl(0, 0%, 95%)"
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "black";
-    ctx.fillRect(x, y, cellSize, cellSize)
-}
-
-const plotGrid = (ctx, size, cellSize) => {
-    const rowSize = size/cellSize
-    const colSize = rowSize
-
-    for (let x = 0; x < rowSize; x++) {
-        for (let y = 0; y < colSize; y++) {
-            plotCell(ctx, cellSize, {x:x*cellSize,y:y*cellSize})          
-        }        
-    }    
 }
 
 export default Whiteboard;
